9d1bb7270f930da7d01905f3727fedfc1362a008
Tonidev <antoniodev2709@gmail.com>
2024-11-20 09:28:45 -0300

    feat: add transactions type

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
-import { dark } from "@clerk/themes";
 import { redirect } from "next/navigation";
 
 const Home = async () => {
@@ -11,13 +10,9 @@ const Home = async () => {
 
   return (
     <div className="flex h-full items-center justify-center">
-      <UserButton
-        showName
-        appearance={{
-          baseTheme: dark,
-        }}
-      />
+      <UserButton showName />
     </div>
   );
 };
+
 export default Home;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,25 @@
-const TransactionsPage = () => {
-  return <h1>Transactions</h1>;
+import { db } from "../_lib/prisma";
+import { Button } from "../_components/ui/button";
+import { ArrowDownUp } from "lucide-react";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
+
+const TransactionsPage = async () => {
+  //acessar as transações do meu banco de dados
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      {/* TÍTULO E BOTÃO */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar transação
+          <ArrowDownUp />
+        </Button>
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
 };
 
 export default TransactionsPage;
